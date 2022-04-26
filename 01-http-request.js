63ab9586fc0a70006f2ec7cababeaeda6f15a0f4
Warren <hello@warren.codes>
2022-04-26 11:35:56 -0600

    Update example; fix https request, add .end().

--- 01-http-request.js
+++ 01-http-request.js
@@ -3,10 +3,22 @@ const https = require('https');
 const options = {
     hostname: 'duckduckgo.com',
     path: '/',
-    port: 80,
+    port: 443,
     method: 'GET'
 };
 
+// Making the request (requires req.end() to finalize and send.)
 const req = https.request(options, (res) => {
-    console.log('HTTP request result object:', res);
+    // console.log('HTTP request result object:', res);
+
+    res.on('data', (data) => {
+        process.stdout.write(data); // Writing data, if received, to the terminal.
+    });
 });
+
+// Output any errors.
+req.on('error', (error) => {
+    console.error(error);
+});
+
+req.end(); // This ends and sends the request.
