625c5ffee1a578c66ce5addf4eeefda0f7bad750
Warren <hello@warren.codes>
2022-06-21 11:58:10 -0600

    Display response data in client program example.

--- 01-http-request.js
+++ 01-http-request.js
@@ -4,15 +4,23 @@
 const https = require('https');
 
 const options = {
-    hostname: 'duckduckgo.com',
-    path: '/about',
+    // hostname: 'duckduckgo.com',
+    hostname: 'cat-fact.herokuapp.com',
+    // path: '/about',
+    path: '/facts',
     port: 443, // Use :80 if it is http
     method: 'GET'
 };
 
 // Send the request!
 const request = https.request(options, (response) => {
-    console.log(response);
+    // console.log(response.headers);
+
+    response.on('data', (data) => {
+        // console.log(data);
+
+        process.stdout.write(data);
+    });
 });
 
 // Check for errors.
