279bbfce616136f824e6d12e3835ebfc61cf37d0
Warren <hello@warren.codes>
2022-07-11 10:14:46 -0600

    Add middleware example.

---
+++ 06-express-with-middleware.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Configuration
+/////////////////////////////////////////////////////////////////////////////////////
+
+const PORT = 8080;
+const app = express();
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Logging middleware.
+app.use((req, res, next) => {
+    console.log(
+        'Request was received:',
+        req.method, // GET / POST
+        req.url // /home /about /blog/3
+    );
+    next(); // Continue to next middleware!
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Routes
+/////////////////////////////////////////////////////////////////////////////////////
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.contentType('text/html');
+    res.end('<html><head><title>Home Page</title></head><body><h1>Home Page</h1></body></html>');
+});
+
+app.get('/about', (req, res) => {
+    res.status(200);
+    res.contentType('text/html');
+    res.end('<html><head><title>About Page</title></head><body><h1>About Page</h1></body></html>');
+});
+
+app.get('/contact', (req, res) => {
+    res.status(200);
+    res.contentType('text/html');
+    res.end('<html><head><title>Contact Page</title></head><body><h1>Contact Page</h1></body></html>');
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Server Listener
+/////////////////////////////////////////////////////////////////////////////////////
+
+app.listen(PORT, () => {
+    console.log('Express server is listening on port:', PORT);
+});
