1e95af12cf7b878eb87b32867bc4b463ac91b5dc
Warren <hello@warren.codes>
2022-08-08 10:19:26 -0600

    Add basic Express web server example.

--- 06-express-server.js
+++ 06-express-server.js
@@ -1,1 +1,20 @@
 const express = require('express');
+
+const PORT = 5050;
+const app  = express();
+
+app.listen(PORT, () => {
+    console.log(`Express application is now listening: http://localhost:${PORT}`);
+});
+
+app.get(['/', '/home'], (req, res) => {
+    res.status(200);
+    res.contentType('text/html');
+    res.end('<html><head><title>Home Page</title></head><body><h1>Home Page</h1></body></html>');
+});
+
+app.get('/about', (req, res) => {
+    res.status(200);
+    res.contentType('text/html');
+    res.end('<html><head><title>About Page</title></head><body><h1>About Page</h1></body></html>');
+});
