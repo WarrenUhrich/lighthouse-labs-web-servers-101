e2913624236542f0e3768e99f418824906a90d6b
Warren <hello@warren.codes>
2022-10-31 09:25:21 -0600

    Add cat-fact API client example

---
+++ 02-client-2.js
@@ -0,0 +1,32 @@
+const https = require('https');
+
+// https://the-cat-fact.herokuapp.com/api/randomfact
+
+const options = {
+    hostname: 'the-cat-fact.herokuapp.com',
+    path: '/api/randomfact',
+    port: 443, // http: 80; https: 443
+    method: 'GET'
+};
+
+const request = https.request(options, (response) => {
+    response.on('data', (data) => {
+        data = String(data);
+
+        const catFactObj = JSON.parse(data);
+
+        // const fact = catFactObj.data[0].fact;
+
+        const facts = catFactObj.data;
+
+        const fact = facts[0].fact;
+
+        console.log(fact);
+    });
+});
+
+request.on('error', (error) => {
+    console.log(error);
+});
+
+request.end();
